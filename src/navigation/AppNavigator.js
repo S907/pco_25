a1a8587d96b47158022fc5177bc7fc54d54b2d3d
s907 <souvikghosh146@gmail.com>
2025-07-21 13:39:59 +0530

    redux library missing

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,11 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MainScreen from '../screens/mainScr/MainScreen';
+import RtkScreen from '../screens/rtkscreen/RtkScreen';
 
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MainScreen} />
+      <Stack.Screen name="Home-rtk" component={RtkScreen} />
+    </Stack.Navigator>
+  );
+}
 const AppNavigator = () => {
   return (
-    <View>
-      <Text>AppNavigator</Text>
-    </View>
+   <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   )
 }
 
---
+++ src/screens/rtkscreen/RtkScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Rtkscreen = () => {
+  return (
+    <View>
+      <Text>Rtkscreen</Text>
+    </View>
+  )
+}
+
+export default Rtkscreen
+
+const styles = StyleSheet.create({})
